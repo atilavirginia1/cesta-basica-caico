43bddbd1025127e125d18bdb4cfd1fb4618b1a70
Ana Carolina <cscarol20@gmail.com>
2018-09-10 02:40:03 -0300

    Protótipo de tela de relatórios

--- src/pages/relatorios/relatorios.ts
+++ src/pages/relatorios/relatorios.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { DetalhesPesquisaPage } from '../detalhes-pesquisa/detalhes-pesquisa';
 
 /**
  * Generated class for the RelatoriosPage page.
@@ -14,8 +15,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'relatorios.html',
 })
 export class RelatoriosPage {
+  pesquisas: Array<{pesquisa: string, aluno: string, supermercado: string, data_realizacao: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.pesquisas = [];
+    for (let p = 1; p < 6; p++) {
+      this.pesquisas.push({
+      	data_realizacao: 'dd/mm/aa',
+        pesquisa: 'Pesquisa ' + p,
+        aluno: 'Aluno ' + p,
+        supermercado: 'Supermercado ' + p,
+      });
+    }
+  }
+
+  itemTapped(event, p) {
+    // That's right, we're pushing to ourselves!
+    this.navCtrl.push(DetalhesPesquisaPage, {
+      push_item: p
+    });
   }
 
   ionViewDidLoad() {
