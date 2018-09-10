17f108ba014b4e55e142c7cd64be0f11a1e834a6
Ana Carolina <cscarol20@gmail.com>
2018-09-10 02:12:14 -0300

    Tela de solicitações

--- src/pages/solicitacoes/solicitacoes.ts
+++ src/pages/solicitacoes/solicitacoes.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavParams } from 'ionic-angular';
+import { FormGroup } from '@angular/forms';
+import firebase from 'firebase';
 
 /**
  * Generated class for the SolicitacoesPage page.
@@ -14,9 +16,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'solicitacoes.html',
 })
 export class SolicitacoesPage {
+  form: FormGroup;
+  public alunosList:Array<any>;
+	public loadedAlunosList:Array<any>;
+  public alunosRef:firebase.database.Reference;
+  alunos: Array<{nome: string, matricula: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(public navParams: NavParams) {
+    this.alunos = [];
+	    for (let p = 1; p < 6; p++) {
+	      this.alunos.push({
+	        nome: 'Nome ' + p,
+	        matricula: 'Matricula ' + p,
+	      });
+	    }
+
+	  	this.alunosRef = firebase.database().ref('/usuarios');
+	  	this.alunosRef.orderByChild("cargo").equalTo("A").on('value', alunosList => {
+			let alunos = [];
+			alunosList.forEach( aluno => {
+		    alunos.push(aluno.val());
+			return false;
+		  });
+
+		  this.alunosList = alunos;
+		  this.loadedAlunosList = alunos;
+		});
   }
+
+  initializeItems(): void {
+	  this.alunosList = this.loadedAlunosList;
+	}
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SolicitacoesPage');
