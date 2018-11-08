f66bb03fd943132f0440c7c10b05c8237952ecae
Ana Carolina <cscarol20@gmail.com>
2018-11-08 18:55:11 -0200

    Detalhes de pesquisas

--- src/pages/detalhes-pesquisa/detalhes-pesquisa.ts
+++ src/pages/detalhes-pesquisa/detalhes-pesquisa.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { ProvedorProvider } from '../../providers/provedor/provedor';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 /**
  * Generated class for the DetalhesPesquisaPage page.
@@ -15,8 +17,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DetalhesPesquisaPage {
   selectedItem: any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  	this.selectedItem = navParams.get('push_item');
+  pesquisa: any;
+  produto: Array<{id: string, marca:string, medida: string, nome: string}>;
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private toast: ToastController, private provider: ProvedorProvider,
+    public db: AngularFireDatabase) {
+    this.selectedItem = navParams.get('push_item');
+    this.pesquisa = db.list('/pesquisas');
+
   }
 
   ionViewDidLoad() {
