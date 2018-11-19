1d125cf823553b3f637ada13d31b622ed189c125
atilavirginia1 <atilavirginia1@gmail.com>
2018-11-18 23:27:27 -0200

    Correção Cadastro

--- src/providers/provedor/provedor.ts
+++ src/providers/provedor/provedor.ts
@@ -52,7 +52,7 @@ export class ProvedorProvider {
   cadastrar(usuario: any){
     return new Promise((resolve, reject) => {
       this.db.list(this.PATH)
-        .push({ nome: usuario.nome, cargo: usuario.cargo, mat_siape: usuario.mat_siape, usuario: usuario.usuario, senha: usuario.senha, email: usuario.email, ativo: usuario.ativo})
+        .push({ nome: usuario.nome, cargo: usuario.cargo, mat_siape: usuario.mat_siape, senha: usuario.senha, email: usuario.email, ativo: usuario.ativo})
         .then(() => resolve());
     })
   }
@@ -81,7 +81,7 @@ export class ProvedorProvider {
 
       if (usuario.key) {
         this.db.list(this.PATH)
-          .update(usuario.key, { nome: usuario.nome, usuario: usuario.usuario })
+          .update(usuario.key, { nome: usuario.nome })
           .then(() => resolve())
           .catch((e) => reject(e));
       } else {
@@ -114,8 +114,6 @@ export class ProvedorProvider {
         }
       })
     }
-
-
 
  remove(usuario: any) {
     var query = firebase.database().ref(this.PATH).orderByChild("email").equalTo(usuario.email);
